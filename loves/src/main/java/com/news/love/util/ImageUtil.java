2a0264f1301b5a48eef9191e4db6e53807f85036
YJCGIT <1070763759@qq.com>
2017-09-25 21:07:59 +0800

    图片下载工具类

--- loves/src/main/java/com/news/love/util/ImageUtil.java
+++ loves/src/main/java/com/news/love/util/ImageUtil.java
@@ -4,8 +4,45 @@ package com.news.love.util;
  * Created by Administrator on 2017/9/25.
  */
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * 图片下载,链接生成
  */
 public class ImageUtil {
+
+    public static InputStream getInputStreamByGet(String url) {
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url)
+                    .openConnection();
+            conn.setReadTimeout(5000);
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = conn.getInputStream();
+                return inputStream;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void saveData(InputStream is, File file) {
+        try{
+            BufferedInputStream bis = new BufferedInputStream(is);
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len = bis.read(buffer)) != -1) {
+                bos.write(buffer, 0, len);
+                bos.flush();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
